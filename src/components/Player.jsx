8c95e063d9be2a8447c230ad45fe092b7df83382
Luis Eduardo <161559751+LERBritto@users.noreply.github.com>
2025-07-21 23:40:15 -0300

    sidebar-finished

---
+++ src/components/Player.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+        <div className='hidden lg:flex items-center gap-4'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Player
