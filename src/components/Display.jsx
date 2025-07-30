175485a9991e338d3b9dd5194fe43222c2a60199
Luis Eduardo <161559751+LERBritto@users.noreply.github.com>
2025-07-29 23:52:15 -0300

    play-select-song-finished

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -17,7 +17,7 @@ const Display = () => {
       displayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`
     } else {
       displayRef.current.style.background = `#121212`
-    }
+    } 
   })
 
   return (
